de877ac6bb1e2ba30607dc01f72ae002a7a4aaca
Sam Bordo <sambordo1@gmail.com>
2022-09-08 11:44:23 -0400

    added dynamodb querying examples

--- lambda_function.py
+++ lambda_function.py
@@ -1,9 +1,50 @@
 import json
-
-print('loading function')
+import boto3
+import random
+from boto3.dynamodb.conditions import Key, Attr
 
 def lambda_handler(event, context):
     
+    client = boto3.resource('dynamodb')
+    table = client.Table('classicAuthors')
+    
+    # Get item from dynamodb by id
+    
+    response = table.get_item(
+        Key={
+            'author_id': 1 ,
+            'quote_id': 1
+        }
+    )
+    
+    print(response['Item'])
+    
+    # Query by partition key
+    
+    response2 = table.query(
+        KeyConditionExpression=Key('author_id').eq(1)
+    )
+    
+    items = response2['Items']
+    for item in items:
+        print(item)
+    
+    # scan table for author name
+    
+    response3 = table.scan(
+        FilterExpression=Attr('author_name').eq('Dickens')    
+    )
+    
+    items = response3['Items']
+    for item in items:
+        print(item)
+        
+    selection = random.choice(tuple(items))
+    print("Random selection: ")
+    print(selection)
+    
+    
+    # Taking in parameters 
     authorName = event['queryStringParameters']['authorName']
     quote = ""
     
