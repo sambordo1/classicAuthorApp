7ddc5ae6e69550e216ec08f2f611a729b6f51b34
Sam Bordo <sambordo1@gmail.com>
2022-09-13 10:11:37 -0400

    added more quotes json data

--- scripts/batch-upload-json.sh
+++ scripts/batch-upload-json.sh
@@ -2,3 +2,5 @@
 
 aws dynamodb batch-write-item --request-items file://../quotesByAuthor/shakespeareQuotes.json
 aws dynamodb batch-write-item --request-items file://../quotesByAuthor/dickensQuotes.json
+aws dynamodb batch-write-item --request-items file://../quotesByAuthor/twainQuotes.json
+aws dynamodb batch-write-item --request-items file://../quotesByAuthor/AustenQuotes.json
